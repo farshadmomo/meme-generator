a3e6bbf2da75218a4ef9a94ae4ea409ca37c34cf
farshadmomo <momtazfarshad@gmail.com>
2023-09-02 22:19:33 +0330

    form build

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,1 +1,11 @@
-
+export default function Meme() {
+    return (
+        <main className='generatorForm'>
+            <form>
+                <input type="text" placeholder='Top text'/>
+                <input type="text" placeholder='Bottom text'/>
+                <button>Get a new meme image <img src='/images/photoicon.png'/></button>
+            </form>
+        </main>
+    )
+}
