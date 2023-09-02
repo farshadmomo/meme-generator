a848e8d66a02d59b912ce3f866777d9d80780b4f
farshadmomo <momtazfarshad@gmail.com>
2023-09-02 21:22:05 +0330

    first push

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header className='headercontainer'>
+            <img src='/images/memeicon.png' />
+            <h1>Meme Generator</h1>
+            <h4>React course - project 3</h4>
+        </header>
+    )
+}
