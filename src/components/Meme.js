c223732d8723d54f8eb1d618cb78ceaa279d49da
farshadmomo <momtazfarshad@gmail.com>
2023-09-04 21:15:59 +0330

    random meme

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,11 +1,20 @@
+import React from "react";
+import memesData from "../memesData";
+
 export default function Meme() {
+    function getMemeImage() {
+        const memeArrays = memesData.data.memes
+        const randomNum = Math.floor(Math.random()*memeArrays.length)
+        const url = memeArrays[randomNum].url
+        console.log(url)
+    }
     return (
         <main className='generatorForm'>
-            <form>
+            <div>
                 <input type="text" placeholder='Top text'/>
                 <input type="text" placeholder='Bottom text'/>
-                <button>Get a new meme image <img src='/images/photoicon.png'/></button>
-            </form>
+                <button onClick={getMemeImage}>Get a new meme image <img src='/images/photoicon.png'/></button>
+            </div>
         </main>
     )
 }
